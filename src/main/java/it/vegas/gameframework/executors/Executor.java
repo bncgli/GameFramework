a38ef2c536a1ba048093a674541b757f72f1e61c
GiulioBanchini <g.banchini@proximanetwork.it>
2023-08-30 12:08:01 +0200

    Added execution monitor interface

--- src/main/java/it/vegas/gameframework/executors/Executor.java
+++ src/main/java/it/vegas/gameframework/executors/Executor.java
@@ -2,6 +2,7 @@ package it.vegas.gameframework.executors;
 
 import it.vegas.gameframework.contexts.GameContext;
 import it.vegas.gameframework.enums.ActionReturn;
+import it.vegas.gameframework.monitors.Monitor;
 import it.vegas.gameframework.rules.GameRules;
 import it.vegas.gameframework.states.GameState;
 import lombok.Getter;
@@ -17,29 +18,27 @@ public class Executor<C extends GameContext, R extends GameRules> extends GameSt
 
     private GameState<C,R> startingState;
     private GameState<C,R> currentState;
+    private Monitor monitor;
+
+    public void setStartingState(GameState<C, R> startingState) {
+        this.startingState = startingState;
+        monitor.setStateMachine(startingState);
+    }
 
     @Override
     public ActionReturn execute() {
         currentState = startingState;
         while (currentState != null) {
-            enterState();
+            monitor.enterState(currentState);
             ActionReturn result = currentState.execute();
             if (result != ActionReturn.OK) {
                 System.out.println(currentState.getName()+" returned: "+ result + " and exited");
                 return result;
             }
-            exitState();
+            monitor.exitState(currentState);
             currentState = currentState.getNextGameState();
         }
         return ActionReturn.OK;
-    }
-
-    public void enterState() {
-        System.out.printf("Starting: %s\n", currentState.getName());
-    }
-
-    public void exitState() {
-        System.out.printf("Exiting: %s\n", currentState.getName());
     }
 
 }
