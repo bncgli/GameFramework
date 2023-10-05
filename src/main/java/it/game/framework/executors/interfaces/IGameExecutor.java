106b7a4752173b945de97468dfa69b5e18553d2e
GiulioBanchini <g.banchini@proximanetwork.it>
2023-10-05 13:04:24 +0200

    Refactoring of executors and extration of interface

--- src/main/java/it/game/framework/executors/interfaces/IGameExecutor.java
+++ src/main/java/it/game/framework/executors/interfaces/IGameExecutor.java
@@ -1,9 +1,22 @@
 package it.game.framework.executors.interfaces;
 
+import it.game.framework.contexts.GameContext;
 import it.game.framework.exceptions.GameException;
+import it.game.framework.statemachines.StateMachine;
 import it.game.framework.states.GameState;
 
 public interface IGameExecutor {
     void execute();
 
+    GameState getCurrentGameState();
+
+    StateMachine getStateMachine();
+
+    GameContext getContext();
+
+    void setCurrentGameState(GameState currentGameState);
+
+    void setStateMachine(StateMachine stateMachine);
+
+    void setContext(GameContext context);
 }
